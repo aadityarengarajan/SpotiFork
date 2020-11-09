4b3932e39b368d445a482278055bb647f9aa45a6
Aaditya <aadit.xo@gmail.com>
2020-11-09 08:29:41 +0300

    My 10 millionth commit

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ from flask_spotify_auth import getAuth, refreshAuth, getToken
 CLIENT_ID = "4bc7d69d58a74c639d6d7f3b3f664809"
 CLIENT_SECRET = "0dceacbf02d542aa8f23934752b3501c"
 PORT = "5001"
-CALLBACK_URL = "https://sheltered-eyrie-04789.herokuapp.com/"
+CALLBACK_URL = "https://sheltered-eyrie-04789.herokuapp.com"
 SCOPE = "streaming playlist-modify-public user-read-private playlist-read-private playlist-read-collaborative playlist-modify-private"
 TOKEN_DATA = []
 
